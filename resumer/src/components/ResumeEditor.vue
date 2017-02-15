a11f16d9392b0c449d7e2fdb1dcb3c82c6d0b4d3
ReedSun <superreedsun@gmail.com>
2017-02-15 14:37:35 +0800

    add tab

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -2,23 +2,20 @@
 	<div id="resume-editor">
 		<nav>
 			<ol>
-				<li class="active"> x </li>
-				<li> x </li>
-				<li> x </li>
-				<li> x </li>
-				<li> x </li>
-				<li> x </li>
-				<li> x </li>
+			<li v-for="(item,index) in resume.visibleItems"
+				v-bind:class="{active: item === selected}"
+				v-on:click="selected = item"
+			>
+				{{index}}
+			</li>
 			</ol>
 		</nav>
 		<ol class="panels">
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-			<li>5</li>
-			<li>6</li>
-			<li>7</li>
+		<li v-for="item in resume.visibleItems"
+			v-show="item === selected"
+		>
+			{{resume[item]}}	
+		</li>
 		</ol>
 	</div>
 </template>
@@ -27,8 +24,26 @@ export default {
   name: 'ResumeEditor',
   data: function () {
     return {
-
-    }
+		selected: "work history",
+		resume: {
+			visibleItems: ['bio', 'work history', 'education', 'project', 'award', 'contacts', 'others'],
+		bio:  {
+				name: '',
+				city: '',
+				title: ''
+			},
+		'work history': {
+			a:1,
+			b:2,
+			c:3
+		},
+		education: [],
+		project: [],
+		award: [],
+		contacts: [],
+		other: []
+		}
+	}
   }
 }
 </script>
@@ -39,6 +54,7 @@ export default {
 		box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
 		display: flex;
 		flex-direction: row;
+		overflow: hidden;
 		>nav {
 			width: 80px;
 			background: black;
@@ -51,6 +67,7 @@ export default {
 					align-items: center;
 					margin-top: 16px;
 					margin-bottom: 16px;
+					cursor: pointer;
 					&.active {
 						background: white;
 						color: black;
